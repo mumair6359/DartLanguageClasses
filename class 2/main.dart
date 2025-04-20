c2116dba4ed3759ce0b3085803adc039cbf93962
M-umair87 <ramzanbibi6234@gmail.com>
2025-04-20 12:43:34 +0500

    added new list properties

--- class 2/main.dart
+++ class 2/main.dart
@@ -1,3 +1,14 @@
 void main() {
-  print("class 2");
+  List studentsName = ["Owais", "Ali", "Hamza", "Mustafa", "Uzair"];
+  print("Name of Students : ${studentsName}");
+  print("");
+  print("print the first value of list: ${studentsName.first}");
+  print("");
+  print("print the last value of list: ${studentsName.last}");
+  print("");
+  print("print the Specific value of list by taking index number: ${studentsName.elementAt(2)}");
+  print("");
+  print("print the length of list : ${studentsName.length}");
+  print("");
+  print("print the total index of list : ${studentsName.length - 1}");
 }
