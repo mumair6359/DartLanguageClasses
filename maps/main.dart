47bc2d64f3498c042faad3d90862282215647444
M-umair87 <ramzanbibi6234@gmail.com>
2025-04-20 14:14:52 +0500

    map  methods

---
+++ maps/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  // MAPS
+  Map studentInfo = {
+    "Name": "Umair",
+    "FName": "Ubaid",
+    "Class": "Flutter",
+    "Sub": "Dart Lang",
+  };
+  print(studentInfo);
+  print(studentInfo.keys); //  to print keyes use .keys
+  print(studentInfo.values); // to print values use .values
+  print(studentInfo["Name"]); // to print specific key
+}
