4a613fc5931782656590abb249d193fcc0e8ad7f
M-umair87 <ramzanbibi6234@gmail.com>
2025-05-11 13:42:20 +0500

    functions

---
+++ maps/Functions.dart
@@ -0,0 +1,53 @@
+void main() {
+  print("Function");
+  // addNumbers() {
+  //   // Function name
+  //   int num1 = 45;
+  //   int num2 = 35;
+  //   int result = num1 + num2;
+  //   print("The answer is : $result");
+  //   return result;
+  // }
+
+  // printMyNameAndAge() {
+  //   // functions
+  //   print("Name is : Muhammad Umair");
+  //   print("Age is : 35");
+  //   print("Cell Number : 03452232548");
+  // }
+
+  // vehicalInfo() {
+  //   // function name
+  //   print("Motor Bike");
+  //   print("Bike Num : KSC-1234");
+  // }
+
+  // int result = addNumbers();
+  // print("");
+  // printMyNameAndAge();
+  // print("");
+  // vehicalInfo();
+  // print("");
+  // print("Functions with parameters");
+  // addNumbers(num1, num2, num3) {
+  //   return num1 + num2 + num3;
+  // }
+  // int totalNum = addNumbers(2, 5, 7);
+  // print("the sum of given numbers : $totalNum");
+
+  // Map bioData() {
+  //   Map name = {"Name": ['Umer', 'FName','javed']};
+  //   return name;
+  // }
+
+  // Map myData = bioData();
+  // print("${myData.values}");
+
+  addNumbers() {
+    var theSum = 2 + 2;
+    return theSum;
+  }
+
+  int result = addNumbers();
+  print(result);
+}
--- maps/loops.dart
+++ maps/loops.dart
@@ -1,56 +1,56 @@
-import 'dart:io';
-import 'dart:math';
+// import 'dart:io';
+// import 'dart:math';
 
-void main() {
-  // For loop
-  // class task
-  // 1 to 100 print odd number
-  // print tables
-  // loop for list
+// void main() {
+//   // For loop
+//   // class task
+//   // 1 to 100 print odd number
+//   // print tables
+//   // loop for list
 
-  List stdname = ["umer", "umair", "aftab", "ali"];
+//   List stdname = ["umer", "umair", "aftab", "ali"];
 
-  // for (var i = 0; i <stdname.length; i++) {
-  //   print("list of student : ${stdname[i]}");
-  // =========================================
-  // for in loop
-  // for (var element in stdname) {
-  //   print(element);
-  // }
+//   // for (var i = 0; i <stdname.length; i++) {
+//   //   print("list of student : ${stdname[i]}");
+//   // =========================================
+//   // for in loop
+//   // for (var element in stdname) {
+//   //   print(element);
+//   // }
 
-  // while loop
+//   // while loop
 
-  // int i = 8;
-  // while (i <= 8) {
-  //   print("While loop");
-  //   i++;
-  // }
+//   // int i = 8;
+//   // while (i <= 8) {
+//   //   print("While loop");
+//   //   i++;
+//   // }
 
-  // email and password input
-  print("Enter your Email Address");
-  String? email = stdin.readLineSync();
-  print("");
-  print("Enter your password");
-  String? password = stdin.readLineSync();
+//   // email and password input
+//   print("Enter your Email Address");
+//   String? email = stdin.readLineSync();
+//   print("");
+//   print("Enter your password");
+//   String? password = stdin.readLineSync();
   
-  var login = true;
-  while (login == true) {
-    if (email == "abc" && password == "123") {
-      print("login SuccessFull");
-      login = false;
-    } else {
-      print("login faild");
-    }
-      break;
+//   var login = true;
+//   while (login == true) {
+//     if (email == "abc" && password == "123") {
+//       print("login SuccessFull");
+//       login = false;
+//     } else {
+//       print("login faild");
+//     }
+//       break;
       
-    }
-  }
+//     }
+//   }
 
-  // do while loop
+//   // do while loop
 
-  // int i = 1;
-  // do {
-  //   print("do while loop");
-  //   i++;
-  // } while (i < 1);
-// }
+//   // int i = 1;
+//   // do {
+//   //   print("do while loop");
+//   //   i++;
+//   // } while (i < 1);
+// // }
